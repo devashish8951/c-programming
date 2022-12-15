d21f17d57ac265c10c13d7a3bd0c440866685742
devashish8951 <119407611+devashish8951@users.noreply.github.com>
2022-12-15 01:24:23 -0800

    Add files via upload
    Pure program

---
+++ Character.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main()
+
+{
+	char c ='b';
+	printf("The character value is : %c\n",c);
+}
---
+++ Primary.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void main()
+{
+int i= 5;
+printf("The integer value is:%d\n",i);
+}
